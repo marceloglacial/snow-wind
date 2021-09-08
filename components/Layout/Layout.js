a08e70d6d265e6d9d4ee660a02e6092ab19e87a4
Marcelo <glacial@gmail.com>
2021-09-08 00:29:30 -0400

    add google font pre-connect

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -17,12 +17,12 @@ export default function Layout({ children }) {
         <meta name='robots' content='index, follow' />
         <meta name='description' content={siteInfo.description} />
         <meta name='keywords' content={siteInfo.keywords} />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
           crossOrigin='anonymous'
           href={`https://fonts.googleapis.com/css?family=${siteInfo.fonts}&display=swap`}
           rel='stylesheet'
         />
-
         <meta property='og:title' content={siteInfo.title} />
         <meta property='og:description' content={siteInfo.description} />
         <meta property='og:image' content='tile-wide.png' />
