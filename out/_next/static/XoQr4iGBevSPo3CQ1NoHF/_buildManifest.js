5939da77c82b176c999e48c065780bae377c9d9d
Marcelo <glacial@gmail.com>
2021-09-08 21:01:48 -0400

    add tests

---
+++ out/_next/static/XoQr4iGBevSPo3CQ1NoHF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f54d76d5d5a65cf396de.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
